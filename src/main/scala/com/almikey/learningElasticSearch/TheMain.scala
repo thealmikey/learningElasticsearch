44f587bc266d9237e1fc6b29ce4a9bf369001977
TheAlmikey <thealmikey@gmail.com>
2018-04-01 18:28:50 +0300

    query fields with continent africa

--- src/main/scala/com/almikey/learningElasticSearch/TheMain.scala
+++ src/main/scala/com/almikey/learningElasticSearch/TheMain.scala
@@ -4,39 +4,23 @@ import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.http.HttpClient
 import com.sksamuel.elastic4s.http.ElasticDsl._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Success, Failure}
+
 object TheMain extends App {
 
   val client = HttpClient(ElasticsearchClientUri("localhost", 9200))
 
 //  client.execute(createIndex("places"))
 
-  client.execute(
-    bulk(
-      indexInto("places" / "cities") id "kny" fields (
-        "name" -> "Nairobi",
-        "country" -> "Kenya",
-        "continent" -> "Africa",
-        "status" -> "Safari"
-      ),
-      indexInto("places" / "cities") id "ug" fields (
-        "name" -> "Kampala",
-        "country" -> "Uganda",
-        "continent" -> "Africa",
-        "status" -> "Land of museveni"
-      ),
-      indexInto("places" / "cities") id "tz" fields (
-        "name" -> "Dodoma",
-        "country" -> "Tanzania",
-        "continent" -> "Africa",
-        "status" -> "Land of Magufuli"
-      ),
-        indexInto("places" / "cities") id "us" fields (
-        "name" -> "Washington",
-        "country" -> "USA",
-        "continent" -> "America",
-        "status" -> "Land of Trump"
-      )
-    )
+  val resp = client.execute(
+    search("places" / "cities").query("africa")
   )
 
+  resp.onComplete(
+    x => x match {
+      case Success(Right(something))=>print(something)
+      case Failure(e)=>println(e.getMessage)
+    }
+  )
 }
