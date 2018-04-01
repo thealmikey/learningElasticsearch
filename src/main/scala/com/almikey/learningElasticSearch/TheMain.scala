efa47a08d1c01698b464de6ec64d36ee84e51a11
TheAlmikey <thealmikey@gmail.com>
2018-04-01 20:16:17 +0300

    able to convert results from elasticsearch to a City case class

--- src/main/scala/com/almikey/learningElasticSearch/TheMain.scala
+++ src/main/scala/com/almikey/learningElasticSearch/TheMain.scala
@@ -1,17 +1,30 @@
 package com.almikey.learningElasticSearch
 
-import com.sksamuel.elastic4s.ElasticsearchClientUri
+import com.sksamuel.elastic4s.{ElasticsearchClientUri, Hit, HitReader}
 import com.sksamuel.elastic4s.http.HttpClient
 import com.sksamuel.elastic4s.http.ElasticDsl._
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{Success, Failure}
+import scala.util.{Failure, Success}
 
 object TheMain extends App {
 
   val client = HttpClient(ElasticsearchClientUri("localhost", 9200))
 
-//  client.execute(createIndex("places"))
+  case class City(name: String,
+                  country: String,
+                  continent: String,
+                  status: String)
+
+  implicit object cityHitReader extends HitReader[City] {
+    override def read(hit: Hit): Either[Throwable, City] = {
+      Right(
+        City(hit.sourceAsMap("name").toString,
+             hit.sourceAsMap("country").toString,
+             hit.sourceAsMap("continent").toString,
+             hit.sourceAsMap("status").toString))
+    }
+  }
 
   val resp = client.execute(
     search("places" / "cities").query("africa")
@@ -19,8 +32,9 @@ object TheMain extends App {
 
   resp.onComplete(
     x => x match {
-      case Success(Right(something))=>print(something)
+      case Success(Right(something))=>print(something.result.to[City])
       case Failure(e)=>println(e.getMessage)
     }
   )
+
 }
