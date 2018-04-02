53820a3d9071d37618b4f214eeada87f1d6146d2
TheAlmikey <thealmikey@gmail.com>
2018-04-02 12:00:00 +0300

    create index, bulk insert and then query

--- src/main/scala/com/almikey/learningElasticSearch/TheMain.scala
+++ src/main/scala/com/almikey/learningElasticSearch/TheMain.scala
@@ -10,6 +10,38 @@ import scala.util.{Failure, Success}
 object TheMain extends App {
 
   val client = HttpClient(ElasticsearchClientUri("localhost", 9200))
+  client
+    .execute(createIndex("places"))
+    .onComplete(_ => {
+      client.execute(
+        bulk(
+          indexInto("places" / "cities") id "kny" fields (
+            "name" -> "Nairobi",
+            "country" -> "Kenya",
+            "continent" -> "Africa",
+            "status" -> "Safari"
+          ),
+          indexInto("places" / "cities") id "ug" fields (
+            "name" -> "Kampala",
+            "country" -> "Uganda",
+            "continent" -> "Africa",
+            "status" -> "Land of museveni"
+          ),
+          indexInto("places" / "cities") id "tz" fields (
+            "name" -> "Dodoma",
+            "country" -> "Tanzania",
+            "continent" -> "Africa",
+            "status" -> "Land of Magufuli"
+          ),
+          indexInto("places" / "cities") id "us" fields (
+            "name" -> "Washington",
+            "country" -> "USA",
+            "continent" -> "America",
+            "status" -> "Land of Trump"
+          )
+        )
+      )
+    })
 
   case class City(name: String,
                   country: String,
@@ -27,13 +59,14 @@ object TheMain extends App {
   }
 
   val resp = client.execute(
-    search("places" / "cities").query("africa")
+    search("places" / "cities").query("nairobi")
   )
 
   resp.onComplete(
-    x => x match {
-      case Success(Right(something))=>print(something.result.to[City])
-      case Failure(e)=>println(e.getMessage)
+    x =>
+      x match {
+        case Success(Right(something)) => print(something.result.to[City])
+        case Failure(e)                => println(e.getMessage)
     }
   )
 
